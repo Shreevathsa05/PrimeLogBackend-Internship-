15c4f7bf6ed5cdedd4f3bdcc49a543fa44807ea6
Shreevathsa05 <bhatshreevathsa17@gmail.com>
2025-03-12 13:49:49 +0530

    Structure Ready to start

---
+++ src/main/java/com/PrimeLog/IMS/InformationManagenetSystemApplication.java
@@ -0,0 +1,13 @@
+package com.PrimeLog.IMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationManagenetSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationManagenetSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PrimeLog/IMS/InformationManagenetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PrimeLog.IMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationManagenetSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
